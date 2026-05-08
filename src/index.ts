6f4cbc72e1fc25c8c16e60632dea3d00ab8d3db4
Rui Rocha <ruirocha0@hotmail.com>
2026-05-08 15:31:08 +0100

    Removed old rest endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { Hono } from "hono";
-import arrangements from "./routes/arrangements.js";
 import generate from "./routes/generate.js";
 import prompt from "./routes/prompt.js";
-import prompts from "./routes/prompts.js";
 
 const app = new Hono();
 
 app.route("/generate", generate);
-app.route("/prompts", prompts);
-app.route("/arrangements", arrangements);
 app.route("/prompt", prompt);
 
 export default app;
